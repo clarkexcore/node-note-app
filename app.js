b95645a50bdd684ac325e3ae7c1efefb75bbc4d8
Alex Clarke <alexclarke@Alexs-MacBook-Pro.local>
2019-10-03 11:59:00 -0400

    Added Yargs to the project

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-const getNotes = require('./notes.js');
+//Modules
 const chalk = require('chalk');
+const yargs = require('yargs');
+//Created Files.
+const getNotes = require('./notes.js');
 
-const green = chalk.bold.red.inverse("Success!!!!");
+//Customize Yargs version
+yargs.version("1.1.0");
 
-const msg = getNotes();
-console.log(msg);
-console.log(green);
+//Create add command
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    handler: function(){
+        console.log("Adding a new note");
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function(){
+        console.log("Removing the note");
+    }
+})
+
+//Create a read command
+yargs.command({
+    command: "read",
+    describe: "Reading a note",
+    handler: function(){
+        console.log("Reading a note");
+    }
+})
+
+//Create a listing command
+yargs.command({
+    command: "list",
+    describe: "List all notes",
+    handler: function(){
+        console.log("List all the notes");
+    }
+})
+
+// console.log(process.argv);
+console.log(yargs.argv);
